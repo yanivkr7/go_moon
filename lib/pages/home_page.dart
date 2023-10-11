0783261c5eadcbd8dca8f03c75aa211cbde7f1d7
Yaniv_Kremer <yanivkr7@gmail.com>
2023-10-11 13:02:52 +0000

    Add widget

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: _pageTitle()));
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        fontSize: 70,
+        color: Colors.white,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage("assets/images/astro_moon.png"),
+        ),
+      ),
+    );
+  }
+}
