ca4ab53ddb7979118dcf281e1936a0208eebae80
Yaniv_Kremer <yanivkr7@gmail.com>
2023-10-11 14:30:47 +0000

    Title

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  late double _deviceHight, _deviceWidth;
+
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: _pageTitle()));
+    _deviceHight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: _deviceHight,
+          width: _deviceWidth,
+          child: _pageTitle(),
+        ),
+      ),
+    );
   }
 
   Widget _pageTitle() {
